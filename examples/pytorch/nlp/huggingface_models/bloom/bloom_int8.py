bf5a5b84d0c53184e291a8a0462ccc8c3ad1f512
Xin He <xin3.he@intel.com>
2023-02-08 14:36:40 +0800

    add int8 bloom
    Signed-off-by: Xin He <xin3.he@intel.com>

---
+++ examples/pytorch/nlp/huggingface_models/bloom/bloom_int8.py
@@ -0,0 +1,85 @@
+import torch
+import transformers
+#from smoothquant.smooth import smooth_lm
+from tqdm import tqdm
+from datasets import load_dataset
+
+
+class Evaluator:
+    def __init__(self, dataset, tokenizer, device):
+        self.dataset = dataset
+        self.tokenizer = tokenizer
+        self.device = device
+
+        # tokenize the dataset
+        def tokenize_function(examples):
+            example = self.tokenizer(examples['text'])
+            return example
+
+        self.dataset = self.dataset.map(tokenize_function, batched=True)
+        self.dataset.set_format(type='torch', columns=['input_ids'])
+
+    @torch.no_grad()
+    def evaluate(self, model):
+        model.eval()
+        # The task is to predict the last word of the input.
+        total, hit = 0, 0
+        for batch in tqdm(self.dataset):
+            input_ids = batch['input_ids'].to(self.device).unsqueeze(0)
+            label = input_ids[:, -1]
+            # attention_mask = torch.ones_like(input_ids)
+            outputs = model(input_ids)
+            last_token_logits = outputs[0]
+            pred = last_token_logits.argmax(dim=-1)
+            total += label.size(0)
+            hit += (pred == label).sum().item()
+        acc = hit / total
+        return acc
+
+
+class CalibDataloader():
+    def __init__(self, dataset, tokenizer, device):
+        self.dataset = dataset
+        self.tokenizer = tokenizer
+        self.device = device
+        self.batch_size = 1
+        # tokenize the dataset
+        def tokenize_function(examples):
+            example = self.tokenizer(examples['text'])
+            return example
+
+        self.dataset = self.dataset.map(tokenize_function, batched=True)
+        self.dataset.set_format(type='torch', columns=['input_ids'])
+
+    def __iter__(self):
+        for batch in self.dataset:
+            input_ids = batch['input_ids'].to(self.device).unsqueeze(0)
+            yield input_ids
+
+
+model_name = "bigscience/bloom-560m"
+
+tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+dataset = load_dataset('lambada', split='validation')
+
+calib_dataloader = CalibDataloader(dataset, tokenizer, 'cpu')
+
+evaluator = Evaluator(dataset, tokenizer, 'cpu')
+
+model = transformers.AutoModelForCausalLM.from_pretrained(
+    model_name, 
+    torchscript=True, # torchscript will force `return_dict=False` to avoid jit errors
+)
+
+def eval_func(model):
+    acc = evaluator.evaluate(model)
+    return acc
+
+from neural_compressor import PostTrainingQuantConfig
+from neural_compressor import quantization
+conf = PostTrainingQuantConfig(backend='ipex')
+q_model = quantization.fit(model,
+                            conf,
+                            calib_dataloader=calib_dataloader,
+                            eval_func=eval_func)
+
