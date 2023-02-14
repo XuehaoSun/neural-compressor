9c0d1c72739e8151e7946a140104d2c0f0e027cd
wenhuach21 <wenhua.cheng@intel.com>
2023-02-14 15:10:44 +0800

    add new fuse patterns add model recovering to support tuning alpha

--- neural_compressor/adaptor/torch_utils/sq.py
+++ neural_compressor/adaptor/torch_utils/sq.py
@@ -1,4 +1,5 @@
 import torch
+from ...utils import logger
 
 
 def model_forward(model, dataloader, sample_cnt):
@@ -24,20 +25,22 @@ def model_forward(model, dataloader, sample_cnt):
 class SmoothQuant:
     def __init__(self, model: torch.nn.Module, dataloader, traced_model=None):
         self.model = model
-        device = self.get_device()
+        device, dtype = self.get_device()
         self.model = self.model.to(device)
         self.model.eval()
         self.device = device
+        self.dtype = dtype
         self.dataloader = dataloader
-        self.input_maxs = {}
+        self.input_maxes = {}
         self.traced_model = traced_model
         if self.traced_model == None:
             self.traced_model = self.model
-        self.transform_cnt = 0
+        self.weight_scale_info = {}
+        self.absorb_scales_info = {}
 
     def get_device(self):
         for _, p in self.model.named_parameters():
-            return p.data.device
+            return p.data.device, p.data.dtype
 
     def get_module(self, key):
         attrs = key.split('.')
@@ -52,15 +55,15 @@ class SmoothQuant:
 
     def save_input_pc_hook(self, name):
         def save_input_hook(model, inputs, outputs):
-            if name not in self.input_maxs.keys():
-                self.input_maxs[name] = []
+            if name not in self.input_maxes.keys():
+                self.input_maxes[name] = []
             input = inputs[0]
             ##TODO check input channel is correct
             if len(model.weight.shape) == 4:  ##conv3d or conv1d not suppoted now, need better way
                 input = input.permute(0, 2, 3, 1)
             input = input.reshape(-1, input.shape[-1])
             max_tensor = torch.max(input, dim=0)[0]
-            self.input_maxs[name].append(max_tensor)
+            self.input_maxes[name].append(max_tensor)
 
         return save_input_hook
 
@@ -96,8 +99,7 @@ class SmoothQuant:
     #     result = t.view(-1).kthvalue(k).values.item()
     #     return result
 
-    def calibration(self, calib_size):
-        absorb_to_layer = self.get_layer_mapping()
+    def calibration(self, absorb_to_layer, calib_num):
         layer_to_absorb = {}
         for key in absorb_to_layer:
             for layer_name in absorb_to_layer[key]:
@@ -111,126 +113,207 @@ class SmoothQuant:
                                                                  torch.nn.Conv2d):
                 if isinstance(module, torch.nn.Conv2d):
                     if module.groups > 1 and module.in_channels == module.out_channels and module.groups == module.in_channels:
-                        pass
-                    else:
                         continue
+                    else:
+                        pass
 
                 hook_modules[name] = module
         if len(hook_modules) == 0:
-            return self.model
+            return {}
 
         self.add_observer(hook_modules)
-        self.dump_min_max(calib_size=calib_size)
+        self.dump_min_max(calib_num=calib_num)
         self.remove_observer()
-        return hook_modules, layer_to_absorb, absorb_to_layer
+        return self.input_maxes
 
-    def dump_min_max(self, calibration_method="min_max", calib_size=100):
-        model_forward(self.model, self.dataloader, calib_size)
+    def dump_min_max(self, calibration_method="min_max", calib_num=100):
+        model_forward(self.model, self.dataloader, calib_num)
         ##stack
-        for key in self.input_maxs.keys():
-            val = self.input_maxs[key]
+        for key in self.input_maxes.keys():
+            val = self.input_maxes[key]
             val = torch.stack(val, dim=0)
             val = torch.max(torch.abs(val), dim=0)[0]  ##FIXME should add abs
-            self.input_maxs[key] = val
+            self.input_maxes[key] = val
 
-    def adjust_parameters(self, absorb_to_layer, input_maxs, alpha=0.5):
-        norm_to_input_maxs = {}
+    def reshape_in_channel_to_last(self, layer_name):
+        weight = self.get_module(layer_name).weight  ##TODO oc*ic, support transposed conv
+        if len(weight.shape) == 4:
+            weight = weight.permute(0, 2, 3, 1)
+            weight = weight.reshape(-1, weight.shape[-1])
+        return weight
+
+    def scale_layer_weight(self, layer_name, scale: torch.Tensor):
+        layer = self.get_module(layer_name)
+        if isinstance(layer, torch.nn.Conv2d):
+            scale = scale.view(1, scale.shape[0], 1, 1)
+            layer.weight *= scale
+        elif isinstance(layer, torch.nn.Linear):
+            layer.weight *= scale
+        else:
+            logger.warning(f"found unsupported layer {type(layer)}, try to multiply scale directly ")
+            layer.weight *= scale
+
+    def absorb_scales(self, layer_name, scale):
+        layer = self.get_module(layer_name)
+        if isinstance(layer, torch.nn.BatchNorm2d):
+            if layer.affine:
+                layer.weight *= scale
+                layer.bias *= scale
+            else:
+                layer.affine = True
+                weight = torch.ones(layer.num_features, device=self.device, dtype=self.dtype) * scale
+                layer.weight = torch.nn.Parameter(
+                    weight, requires_grad=False)
+                bias = torch.zeros(layer.num_features, device=self.device, dtype=self.dtype)
+                layer.bias = torch.nn.Parameter(bias, requires_grad=False
+                                                )
+        elif isinstance(layer, torch.nn.LayerNorm):
+            if layer.elementwise_affine:
+                layer.weight *= scale
+                layer.bias *= scale
+            else:
+                layer.elementwise_affine = True
+                weight = torch.ones(layer.num_features, device=self.device, dtype=self.dtype) * scale
+                layer.weight = torch.nn.Parameter(
+                    torch.ones(weight, requires_grad=False))
+                bias = torch.zeros(layer.num_features, device=self.device, dtype=self.dtype)
+                layer.bias = torch.nn.Parameter(
+                    bias, requires_grad=False)
+
+        elif isinstance(layer, torch.nn.Conv2d):
+            ##the order could not be changed
+            if hasattr(layer, "bias") and (layer.bias != None):
+                layer.bias *= scale
+            scale = scale.view(-1, scale.shape[0], 1, 1)
+            layer.weight *= scale
+        elif isinstance(layer, torch.nn.Linear):
+            if hasattr(layer, "bias") and (layer.bias != None):
+                layer.bias *= scale
+            scale = scale.view(-1, scale.shape[0])
+            layer.weight *= scale
+
+        else:
+            logger.warning(f"found unsupported layer {type(layer)}, try to multiply scale to weight and bias directly ")
+            if hasattr(layer, "weight") and layer.weight != None:
+                layer.weight *= scale
+            if hasattr(layer, "bias") and layer.bias != None:
+                layer.bias *= scale
+
+    def adjust_parameters(self, absorb_to_layer, input_maxes, alpha=0.5):
+        absorb_to_input_maxes = {}
         for key in absorb_to_layer.keys():
             layer_name = absorb_to_layer[key][0]
-            norm_to_input_maxs[key] = input_maxs[layer_name]
+            absorb_to_input_maxes[key] = input_maxes[layer_name]
 
+        weight_scales_info = {}
+        absorb_scales_info = {}
         for index, key in enumerate(absorb_to_layer.keys()):
-            input_max = norm_to_input_maxs[key]
+            input_max = absorb_to_input_maxes[key]
             layers = absorb_to_layer[key]
             weights = []
             for layer in layers:
-                weight = self.get_module(layer).weight  ##TODO oc*ic, support transposed conv
-                if len(weight.shape) == 4:
-                    weight = weight.permute(0, 2, 3, 1)
-                    weight = weight.reshpae(-1, weight.shape[-1])
+                weight = self.reshape_in_channel_to_last(layer)
                 weights.append(weight)
 
             weights = torch.cat(weights, dim=0)
 
-            weight_max_per_channel = torch.max(torch.abs(weights), dim=0)[0]  ##FIXME abs
+            weight_max_per_channel = torch.max(torch.abs(weights), dim=0)[0]
             input_power = torch.pow(input_max, alpha)
-            print(max(input_max), min(input_power))
+            logger.info(f"{max(input_max)}, {min(input_power)}")  ##TODO changed it to debug later
             weight_power = torch.pow(weight_max_per_channel, 1 - alpha)
 
             ##adjust parameters
             scale = torch.clip(input_power / weight_power, min=1e-5)
 
-            norm_layer = self.get_module(key)
-            weights_layers = []
-            layers = absorb_to_layer[key]
+            # absorb_layer = self.get_module(key)
 
-            for layer in layers:
-                layer = self.get_module(layer)
-                weights_layers.append(layer)
             ##TODO if norm does not have affine, need to add one
-            norm_layer.weight /= scale
-            norm_layer.bias /= scale
-            for layer in weights_layers:
-                layer.weight *= scale
+            # absorb_layer.weight /= scale
+            # absorb_layer.bias /= scale
+            self.absorb_scales(key, 1.0 / scale)
+            absorb_scales_info[key] = 1.0 / scale
+            layer_names = absorb_to_layer[key]
+            for layer_name in layer_names:
+                self.scale_layer_weight(layer_name, scale)
+                weight_scales_info[layer_name] = scale
+            return weight_scales_info, absorb_scales_info
 
-    def transform(self, alpha=0.5, calib_size=100):
-        if self.transform_cnt > 0:
-            return self.model
+    def transform(self, alpha=0.5, percentile=99.999, op_types=['Linear', 'Conv'], scales_per_op=True, calib_num=100):
+        ##return self.model
+        ##TODO we may call this funtction several times in tuning quantization configuration, we need to revert the change
         with torch.no_grad():
-            hook_modules, layer_to_absorb, absorb_to_layer = self.calibration(calib_size)
+            absorb_to_layer, no_absorb_layers = self.trace(
+                op_types)  ##TODO we need to insert mul layer for no_absorb_layers later
+            if absorb_to_layer == None:
+                logger.warning("sorry, could not trace the model")  ##TODO convert to insert mul mode
+                return self.model
 
-            self.adjust_parameters(absorb_to_layer, self.input_maxs, alpha)
-            self.transform_cnt += 1
+            input_maxes = self.calibration(absorb_to_layer, calib_num)
+
+            self.weight_scale_info, self.absorb_scales_info = self.adjust_parameters(absorb_to_layer, input_maxes,
+                                                                                     alpha)
             return self.model
 
-    def get_layer_mapping(self):
+    def recover(self):
+        with torch.no_grad():
+            for key in self.weight_scale_info:
+                self.scale_layer_weight(key, 1.0 / self.weight_scale_info[key])
+            for key in self.absorb_scales_info:
+                self.absorb_scales(key, 1.0 / self.absorb_scales_info[key])
+
+    def trace(self, op_types):
         tg = TorchGraphAnalysis()
         for idx, input in enumerate(self.dataloader):
             example_inputs = input
+            break
         # for batch in (self.dataset):
         #     example_inputs = batch['input_ids'].to(self.device).unsqueeze(0)
         #     break
 
-        layer_mapping = tg.get_absorb_to_layer(self.traced_model, example_inputs)
-        return layer_mapping
+        absorb_to_layer, no_absorb_layers = tg.get_absorb_to_layer(self.traced_model, example_inputs, op_types)
+        return absorb_to_layer, no_absorb_layers
 
 
 class TorchGraphAnalysis:
     def __init__(self):
         self.aten_to_op = {
-            "aten::linear": "linear",  ##TODO support conv
+            "aten::linear": "Linear",  ##TODO support conv
             "aten::layer_norm": "layer_norm",
             "aten::to": "to",
+            "aten::_convolution": "Conv"
         }
         ##TODO, must statisfy ax=f(ax),current skip layer may be incomplete, matmul/conv->Relu/leakyRelu->Matmul/conv is an option
         self.skip_ops_to_find_abosrb = ["aten::to",
+                                        "aten::relu",
+                                        "aten::leaky_relu"
                                         ##"aten::size",
                                         ##"prim::NumToTensor"
                                         ]
         # self.skip_ops_to_find_abosrb = ["aten::to"]
-        self.could_absorb_layers = ["layer_norm"]  ##TODO,suppport more norm
+        self.could_absorb_layers = ["layer_norm", "batch_norm", "Linear", "Conv"]  ##TODO,suppport more norm
 
     def trace(self, model, dummy_input):
-        ##print(dummy_input)
         traced_model = None
+        optimize_numerics = False
         if isinstance(dummy_input, dict):
             try:
                 traced_model = torch.jit.trace(model, dummy_input["input_ids"], strict=False)
-                traced_model = torch.jit.freeze(traced_model.eval())
+                traced_model = torch.jit.freeze(traced_model.eval(), optimize_numerics=optimize_numerics)
             except:
                 pass
         else:
             try:
                 traced_model = torch.jit.trace(model, dummy_input, strict=False)  ##TODO polish the code
-                traced_model = torch.jit.freeze(traced_model.eval())
+                traced_model = torch.jit.freeze(traced_model.eval(), optimize_numerics=optimize_numerics)
             except:
                 try:
                     traced_model = torch.jit.trace(model, dummy_input[0], strict=False)
-                    traced_model = torch.jit.freeze(traced_model.eval())
+                    traced_model = torch.jit.freeze(traced_model.eval(), optimize_numerics=optimize_numerics)
                 except:
                     assert False, "can't trace the model"
+        return traced_model
 
-        self.traced_model = traced_model  ##TODO,need to check memory usage
+        ##self.traced_model = traced_model  ##TODO,need to check memory usage
 
     def kind_to_op_type(self, kind):
         if kind in self.aten_to_op:
@@ -250,11 +333,11 @@ class TorchGraphAnalysis:
             return None
         return list(node.inputs())[0].node()
 
-    def get_nodes(self, op_types=['linear']):
+    def get_nodes(self, traced_model, op_types=['Linear']):
         if isinstance(op_types, str):
             op_types = [op_types]
         nodes = []
-        for node in self.traced_model.graph.nodes():
+        for node in traced_model.graph.nodes():
             node_type = node.kind()
             for op_type in op_types:
                 if self.kind_to_op_type(node_type) == op_type:
@@ -262,8 +345,8 @@ class TorchGraphAnalysis:
                     break
         return nodes
 
-    def get_prev_norm_layer(self, nodes):  ## TODO may be not correct for other models
-        norm_mapping = []
+    def get_prev_absorb_layer(self, nodes):  ## TODO may be not correct for other models
+        prev_absorb_layer = []
         for node in nodes:
             parent = self.get_parent(node)
             while 1:
@@ -271,27 +354,30 @@ class TorchGraphAnalysis:
                     parent = self.get_parent(parent)
                     continue
                 if self.kind_to_op_type(parent.kind()) in self.could_absorb_layers:
-                    norm_mapping.append(parent)
+                    prev_absorb_layer.append(parent)
                 else:
-                    norm_mapping.append(None)
+                    prev_absorb_layer.append(None)
                 break
-        return norm_mapping
+        return prev_absorb_layer
 
-    def get_absorb_to_layer(self, model, example_input):
-        self.trace(model, example_input)
-        nodes_types = self.get_nodes(["linear"])
+    def get_absorb_to_layer(self, model, example_input, op_types):
+        traced_model = self.trace(model, example_input)
+        if traced_model == None:
+            return None
+        nodes_types = self.get_nodes(traced_model, op_types)
         nodes = [node_type[0] for node_type in nodes_types]
-        nodes_prev_norm = self.get_prev_norm_layer(nodes)
+        nodes_prev_absorb = self.get_prev_absorb_layer(nodes)
         absorb_to_layer = {}
-        for index, norm in enumerate(nodes_prev_norm):
-            if norm == None:
+        no_absorb_layers = []
+        for index, absorb in enumerate(nodes_prev_absorb):
+            if absorb == None:
+                no_absorb_layers.append(nodes[index])
                 continue
             node = nodes[index]
             layer_name = '.'.join(node.scopeName().split('/')[-1].split('.')[1:])
-            norm_name = '.'.join(norm.scopeName().split('/')[-1].split('.')[1:])
-            if norm_name in absorb_to_layer.keys():
-                absorb_to_layer[norm_name].append(layer_name)
+            absorb_name = '.'.join(absorb.scopeName().split('/')[-1].split('.')[1:])
+            if absorb_name in absorb_to_layer.keys():
+                absorb_to_layer[absorb_name].append(layer_name)
             else:
-                absorb_to_layer[norm_name] = [layer_name]
-        ##del self.traced_model
-        return absorb_to_layer
+                absorb_to_layer[absorb_name] = [layer_name]
+        return absorb_to_layer, no_absorb_layers
